d4afe0a126d875cf09ce3a93c0baa038f191ed9a
Kévin Bessière <bessiere.kevin@gmail.com>
2013-06-06 12:59:09 +0200

    Delete library, update integration system

--- src/it/scanpay/scanpay_demo_android/ScanPayTestActivity.java
+++ src/it/scanpay/scanpay_demo_android/ScanPayTestActivity.java
@@ -1,8 +1,8 @@
 package it.scanpay.scanpay_demo_android;
 
+import scanpay.it.SPCreditCard;
+import scanpay.it.ScanPay;
 import scanpay.it.ScanPayActivity;
-import scanpay.it.config.ScanPay;
-import scanpay.it.model.SPCreditCard;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +12,8 @@ import android.widget.Toast;
 
 public class ScanPayTestActivity extends Activity
 {
+    static final int YOUR_RESULT_DEFINE = 4;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -20,29 +22,30 @@ public class ScanPayTestActivity extends Activity
 		setContentView(R.layout.activity_scan_pay_test);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_scan_pay_test, menu);
-		return true;
-	}
-
 	public void startScan(View v)
 	{
 		Intent scanActivity = new Intent(this, ScanPayActivity.class);
 		scanActivity.putExtra(ScanPay.EXTRA_TOKEN, "Enter_your_token_here");
-		startActivityForResult(scanActivity, ScanPay.RESULT_SCANPAY_ACTIVITY);
+
+        //Put true if you want use your own manual entry UI
+        scanActivity.putExtra(ScanPay.EXTRA_USE_CUSTOM_CONFIRMATION_VIEW, false);
+        startActivityForResult(scanActivity, YOUR_RESULT_DEFINE);
 	}
 	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data)
 	{
 	  super.onActivityResult(requestCode, resultCode, data);
-	  if (requestCode == ScanPay.RESULT_SCANPAY_ACTIVITY && resultCode == RESULT_OK)
+      // The result code is ScanPay.RESULT_SCAN_SUCCESS when we succeed to scan
+	  if (requestCode == YOUR_RESULT_DEFINE && resultCode == ScanPay.RESULT_SCAN_SUCCESS)
 	  {
 	    SPCreditCard creditCard = (SPCreditCard) data.getParcelableExtra(ScanPay.EXTRA_CREDITCARD);
-	    Toast.makeText(this, creditCard.getNumber() + " " + creditCard.month + "/" + creditCard.year + " " + creditCard.pictogram, Toast.LENGTH_LONG).show();
+	    Toast.makeText(this, creditCard.number + " " + creditCard.month + "/" + creditCard.year + " " + creditCard.cvv.length(), Toast.LENGTH_LONG).show();
 	  }
+      // The result code is ScanPay.RESULT_SCAN_CANCEL when the user back
+      else if (requestCode == YOUR_RESULT_DEFINE && resultCode == ScanPay.RESULT_SCAN_CANCEL)
+      {
+          Toast.makeText(this, "Scan cancel", Toast.LENGTH_LONG).show();
+      }
 	}
 }
